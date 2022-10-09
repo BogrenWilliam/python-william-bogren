db3b45815df895adfec2174aefaf6ecba1d0fec6
William Bogren <@yahoo.com>
2022-10-09 12:39:10 +0200

    start

---
+++ Labs/Geometry_shapes.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+import math
+from matplotlib.pyplot import plot as plt
+from matplotlib.patches import Rectangle
+from matplotlib.patches import Circle
+import numpy as np
