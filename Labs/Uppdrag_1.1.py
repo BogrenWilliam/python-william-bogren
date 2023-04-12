1065a92c55803e0da2ed2992c1424427da73e310
William Bogren <@yahoo.com>
2023-04-12 22:22:02 +0200

    Done with 1.1

---
+++ Labs/Uppdrag_1.1.py
@@ -0,0 +1,32 @@
+# Be användaren att skriva in sin ålder
+print("skriv din ålder:")
+age = input()
+
+# Be användaren att skriva sitt sparmål 
+print("skriv in ditt sparmål:")
+money_Goal = input()
+
+# Skriv ut informationen
+print(age + " " + money_Goal)
+
+money_goal = 15000
+
+if money_goal > 20000 age < 60; 
+
+   money_goal = money_goal + 500; 
+
+else: 
+    money_goal = money_goal + 1000; 
+
+    age; 
+
+while money_goal ((money_goal < 20000 && age < 65)):
+
+    
+
+
+
+
+
+
+
