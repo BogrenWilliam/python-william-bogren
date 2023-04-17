b30a472cf547de867fb65b5733c786e2abdacfa0
William Bogren <@yahoo.com>
2023-04-17 15:23:43 +0200

    definitions

---
+++ Labs/Uppdrag_1.3.py
@@ -0,0 +1,18 @@
+class Bankaccount: 
+    def __str__(self, name, saldo, månadssparande, aktier): 
+        
+        @property
+        def name(self):
+            return self 
+        
+        def saldo(self): 
+            return self
+        
+        def månadssparande(self): 
+            return self
+
+        def aktier(self): 
+            return self
+                
+
+print(self, name, saldo, månadssparande, aktier):
