d1e2b42946938abb97909d462ba3a8e970da66d9
William Bogren <@yahoo.com>
2023-04-17 16:14:48 +0200

    Aktiepost

---
+++ Labs/Uppdrag_1.4.py
@@ -0,0 +1,19 @@
+class Aktiepost:
+     def __str__(self, värde_vid_köp, värde, datum_för_köp, antal_aktier): 
+     
+     def __init__(self, other: värde_vid_köp):
+
+     def __init__(self, other: värde):
+          
+     def __init__(self, other: datum_för_köp): 
+     
+     def __init__(self, other: antal_aktier):
+          
+    Aktiepost(ett_namn: string, ett_värde:int, ett_antalaktier:int): 
+
+print(Aktiepost)
+
+ 
+
+    
+     
